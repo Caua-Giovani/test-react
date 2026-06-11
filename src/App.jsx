275b3486f96a6ccdd2808f2097aa1657f172c07e
Cauã Giovani <caua.g.andrade7@aluno.senai.br>
2026-06-11 10:18:18 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,26 @@ import './App.css'
 
 function App() {
 
+  const objeto = 
+  {
+    id: 1, 
+    nome:'João'
+  };
+
+  const styles = 
+  {
+    containerFrutas: 'classe-azul',
+    'container-legumes': 'classe-verde'
+  };
 
   return (
     <>
-      
+      <p>Ponto:{objeto.nome}</p>
+      <p>Colchete:{objeto["nome"]}</p>
+
+      <p>Ponto:{styles.containerFrutas}</p>
+      <p>Colchete:{styles['container-legumes']}</p>
+
       <MsgUsuario autenticado={true} nomeUsuario={"SENAI"}/>
       <MsgUsuario nomeUsuario={"SENAI"}/>
 
--- src/components/MensagemUsuario/MsgUsuario.jsx
+++ src/components/MensagemUsuario/MsgUsuario.jsx
@@ -2,10 +2,21 @@ import styles from "./msgUsuario.module.css"
 
 function MsgUsuario({autenticado=false,nomeUsuario}){
 
+
+    const msgAutenticado = <h1 className={styles.autenticado}>Bem Vindo, {nomeUsuario}</h1>
+    const msgNaoAutenticado = <h1 className={styles.naoAutenticado}>Usuario Não Logado</h1>
+
     return(
-        autenticado === true ? <h1 className={styles.autenticado}>Bem Vindo, {nomeUsuario}</h1> : <h1 className={styles.naoAutenticado}>Usuario Não Logado</h1>
+        autenticado === true ? msgAutenticado : msgNaoAutenticado 
     )
 
+
+
+    // return(
+    //     autenticado === true ? <h1 className={styles.autenticado}>Bem Vindo, {nomeUsuario}</h1> : <h1 className={styles.naoAutenticado}>Usuario Não Logado</h1>
+    // )
+
+    
 
     // if (autenticado === true){
     //     return(
