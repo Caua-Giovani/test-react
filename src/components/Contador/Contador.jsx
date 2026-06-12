7b5b72bca0153af34ef4bbddffa9991c6482f392
Cauã Giovani <caua.g.andrade7@aluno.senai.br>
2026-06-12 08:11:51 -0300

    Modify: quase terminando o contador

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,15 +1,34 @@
 import styles from "./contador.module.css"
 import { useState } from "react"
 
+
 function Contador(){
+    const [contador, setContador] = useState(0)
+
+    const adicionar = () =>{
+        setContador(contador+1)
+    }
+    const reiniciar = () =>{
+        setContador(0)
+    }
+    const subtrair = () =>{
+        setContador(contador-1)
+    }
+    const sortear = () =>{
+        setContador(Math.floor(Math.random() * 50))
+    }
+
 
     return(
         <>
-            <p>Aqui vai o contador</p>
+            <p style={{color:contador == 0 ? 'black': contador > 0 ? 'green' : 'red' }}>{contador}</p>
+            <button onClick={reiniciar} >Reiniciar</button>
+            <button onClick={adicionar} >Acrescentar</button>
+            <button onClick={subtrair} >Subtrair</button>
+            <button onClick={sortear} >Sortear</button>
         </>
     )
 }
 
+
 export default Contador
-
-
